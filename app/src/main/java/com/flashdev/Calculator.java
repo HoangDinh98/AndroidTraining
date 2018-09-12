0657d07c41cac54947d56dd7641df490abed9946
PN\hoang.dinh <hoang.dinh@student.passerellesnumeriques.org>
2018-09-12 17:24:13 +0700

    Add funtion for each button in Calculator

--- app/src/main/java/com/flashdev/Calculator.java
+++ app/src/main/java/com/flashdev/Calculator.java
@@ -26,24 +26,71 @@ public class Calculator extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculator);
+        this.screenResult = (TextView) findViewById(R.id.text_result);
 
-        screenResult = (TextView) findViewById(R.id.text_result);
+        this.getGeneralButton();
+        this.getFunctionButton();
 
+        this.setGeneralButtonEvent();
+        this.setFunctionButtonEvent();
+
+
+    }
+
+    protected void getGeneralButton() {
         for (int i = 0; i <= 9; i++) {
-            btn = (Button) findViewById(getResources().getIdentifier("btn_" + i, "id",
+            this.btn = (Button) findViewById(getResources().getIdentifier("btn_" + i, "id",
                     this.getPackageName()));
 
-            this.buttons.add(btn);
+            this.buttons.add(this.btn);
+        }
+
+        this.btn = (Button) findViewById(R.id.btn_plus);
+        this.buttons.add(this.btn);
+        this.btn = (Button) findViewById(R.id.btn_minus);
+        this.buttons.add(this.btn);
+        this.btn = (Button) findViewById(R.id.btn_multi);
+        this.buttons.add(this.btn);
+        this.btn = (Button) findViewById(R.id.btn_div);
+        this.buttons.add(this.btn);
+    }
+
+    protected void getFunctionButton() {
+        this.btn = (Button) findViewById(R.id.btn_ac);
+        this.buttons.add(this.btn);
+        this.btn = (Button) findViewById(R.id.btn_equal);
+        this.buttons.add(this.btn);
+    }
+
+
+    protected void setGeneralButtonEvent() {
+        for (int i = 0; i <= this.buttons.size() - 3; i++) {
             final int index = i;
             this.buttons.get(i).setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     String text = (String) buttons.get(index).getText();
+                    text = (String) screenResult.getText() + text;
                     screenResult.setText(text);
                 }
             });
 
-
         }
+    }
+
+    protected void setFunctionButtonEvent() {
+        this.buttons.get( this.buttons.size()-1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                screenResult.setText("Feature is coming soon!");
+            }
+        });
+
+        this.buttons.get( this.buttons.size()-2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                screenResult.setText("");
+            }
+        });
     }
 }
