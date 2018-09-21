8a4058aa99d989737fc389998b68055e87154b03
PN\hoang.dinh <hoang.dinh@student.passerellesnumeriques.org>
2018-09-21 19:04:39 +0700

    Practice 21-09-2018

--- app/src/main/java/com/flashdev/Login.java
+++ app/src/main/java/com/flashdev/Login.java
@@ -2,13 +2,62 @@ package com.flashdev;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Login extends Activity {
+    protected EditText email, password;
+    protected Button btnLogin;
+    protected String error;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        this.config();
     }
+
+    protected void config() {
+        this.email = (EditText) findViewById(R.id.act_log_edit_phone);
+        this.password = (EditText) findViewById(R.id.act_log_edit_password);
+        this.btnLogin = (Button) findViewById(R.id.act_log_btn_login);
+    }
+
+    protected void setAction() {
+        this.btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkInfo();
+
+                if(error != "") {
+                    Toast.makeText(Login.this, error, Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+            }
+        });
+    }
+
+    protected void checkInfo() {
+        String strEmail = email.getText().toString();
+        String strPassword = password.getText().toString();
+
+        if (strEmail != "admin") {
+            this.error = "Email or Password incorect!";
+            return;
+        }
+
+        if (strPassword != "admin") {
+            this.error = "Password incorect!";
+            return;
+        }
+
+        this.error = "";
+    }
+
+
 
 }
